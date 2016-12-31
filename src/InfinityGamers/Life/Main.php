fda36c250e49793e84f673b828c79ced15b42266
eDroid <edroiiid@gmail.com>
2016-12-31 07:22:39 -0800

    Update Main.php

--- src/InfinityGamers/Life/Main.php
+++ src/InfinityGamers/Life/Main.php
@@ -1,1 +1,15 @@
 <?php
+namespace InfinityGamers\Life;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase {
+ public function onEnable(){ 
+  @mkdir($this->getDataFolder());
+  $this->saveDefaultConfig();
+  $this->config = new Config($this->getDataFolder() . 'config.yml', CONFIG::YAML);
+ }
+}
